bcfa2925c964e68309560e6faa5a162a181193ff
Mirazbd123 <mirazalambd1230@gmail.com>
2024-03-06 11:46:13 +0600

    m

--- template.py
+++ template.py
@@ -41,7 +41,7 @@ for filepath in list_of_files:
         os.makedirs(filedir,exist_ok=True)
         logging.info(f"Creating directory: {filedir} for the file: {filename}")
     
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath == 0)):
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
         with open(filepath , "w") as f:
             pass
             logging.info(f"Creating empty file : {filepath}")
